b6da0a982cb9ca30c12ed32e7c53fb51a3d2dd51
Wind <jp3@jayps.net>
2026-07-27 23:25:59 +0000

    Mon Jul 27 11:25:59 PM UTC 2026

--- home.nix
+++ home.nix
@@ -54,6 +54,12 @@ in
     };
   };
 
+  home.pointerCursor = {
+	name = "phinger-cursors-dark";
+	package = pkgs.phinger-cursors;
+	size = 32;
+	gtk.enable = true;
+  };
 
   services.ssh-agent.enable = true;
 
