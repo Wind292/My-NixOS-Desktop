bcd11227bf65d6f963a795b7f0f644016d19d312
Wind <jp3@jayps.net>
2026-07-28 01:15:02 +0000

    Tue Jul 28 01:15:02 AM UTC 2026

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@
   services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia = {
     modesetting.enable = true;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   }; 
