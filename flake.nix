5e94db56c73340f8a75a26816d57c8cfd01ff6a6
Wind <jp3@jayps.net>
2026-07-30 18:28:20 +0000

    Thu Jul 30 06:28:20 PM UTC 2026

--- flake.nix
+++ flake.nix
@@ -4,11 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    hyprland-preview-share-picker = {
-      url = "github:WhySoBad/hyprland-preview-share-picker";
-      # You may optionally override the nixpkgs input to save space.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    hyprland-preview-share-picker.url = "github:WhySoBad/hyprland-preview-share-picker";
 
     nix-editor.url = "github:snowfallorg/nix-editor";
     nix-editor.inputs.nixpkgs.follows = "nixpkgs";
