9635d4c9666777bb4cd186b8bec8237853da8a39
Wind <jp3@jayps.net>
2026-07-30 17:40:20 +0000

    Thu Jul 30 05:40:20 PM UTC 2026

---
+++ hypr/local.lua
@@ -0,0 +1,12 @@
+hl.monitor({
+    output   = "HDMI-A-2",
+    mode     = "2560x1440@144",
+    position = "0x0",
+    scale    = "auto",
+})
+hl.monitor({
+    output   = "DP-2",
+    mode     = "preferred",
+    position = "1080x0",
+    scale    = "0.75",
+})
