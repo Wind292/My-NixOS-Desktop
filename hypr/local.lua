b44ca72332308d4523030e9b6ba55f36e90f9015
Wind <jp3@jayps.net>
2026-07-30 20:40:12 -0400

    Thu Jul 30 08:40:12 PM EDT 2026

--- hypr/local.lua
+++ hypr/local.lua
@@ -3,10 +3,12 @@ hl.monitor({
     mode     = "2560x1440@144",
     position = "0x0",
     scale    = "auto",
+    bitdepth = 10
 })
 hl.monitor({
     output   = "DP-1",
     mode     = "3440x1440@100",
     position = "2560x0",
     scale    = "1.0",
+    bitdepth = 10,
 })
