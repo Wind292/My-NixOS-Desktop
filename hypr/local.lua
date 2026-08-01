68f27325878af97a23bd77afee03d013b8dc1ae1
Wind <jp3@jayps.net>
2026-08-01 16:37:19 -0400

    Sat Aug  1 04:37:19 PM EDT 2026

--- hypr/local.lua
+++ hypr/local.lua
@@ -1,14 +1,14 @@
 hl.monitor({
     output   = "HDMI-A-2",
     mode     = "2560x1440@144",
-    position = "0x0",
+    position = "2560x0",
     scale    = "auto",
     bitdepth = 10
 })
 hl.monitor({
     output   = "DP-2",
     mode     = "1920x1080@60",
-    position = "2560x0",
+    position = "0x0",
     scale    = "0.75",
     bitdepth = 10,
 })
