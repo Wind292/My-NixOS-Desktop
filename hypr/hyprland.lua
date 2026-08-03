7321668e649b8343bee82e0ac0f79f34030486e8
Wind <jp3@jayps.net>
2026-08-03 16:46:00 -0400

    Mon Aug  3 04:46:00 PM EDT 2026

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -13,7 +13,7 @@
 
 -- You can (and should!!) split this configuration into multiple files
 -- Create your files separately and then require them like this:
-require("./local.lua")
+require("~/.config/hypr/local.lua")
 
 
 ------------------
